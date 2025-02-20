bac567e972fe6a713cd5d9d038e63bcd863a4c9b
Aman Kumar <ak1223125@gmail.com>
2025-02-20 21:00:09 +0530

    Insertion sort

--- InsertionSortAlgorithm.java
+++ InsertionSortAlgorithm.java
@@ -4,13 +4,14 @@ public class InsertionSortAlgorithm {
 
     static int[] Insertion_Sort(int [] arr){
 
-        for(int i = 0; i < arr.length-1; i++){
-            for(int j = i+1; j < arr.length; j++){
-                if(arr[i] > arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+        for(int i = 1; i < arr.length; i++){
+            int j = i;
+
+            while(j > 0 && arr[j] < arr[j-1]){
+                int temp = arr[j-1];
+                arr[j-1] = arr[j];
+                arr[j] = temp;
+                j = j - 1;
             }
         }
 
