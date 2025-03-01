226e2390d92f17018a7b4842670fa966ed5d4eee
Aman Kumar <ak1223125@gmail.com>
2025-03-01 18:11:10 +0530

    For loop decrement by 2

---
+++ ForLoopDecrement.java
@@ -0,0 +1,8 @@
+public class ForLoopDecrement {
+    public static void main(String[] args) {
+        
+        for(int i = 20; i >= 2; i-=2){
+            System.out.print(i+" ");
+        }
+    }
+}
