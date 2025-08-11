7f7293221983cd9f802f3f7dd6a3e7e548a1d2ae
Aman Kumar <ak1223125@gmail.com>
2025-08-11 12:36:44 +0530

    Linked List Basics

---
+++ LinkedLiistBasic.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Node{
+    int data;
+    Node next;
+
+    Node(int data){
+        this.data = data;
+    }
+}
+
+public class LinkedLiistBasic {
+
+    public static void printLinkedList(Node head){
+        Node temp = head;
+        for(int i = 1; i <= 5; i++){
+           System.out.print(temp.data+" ");
+           temp = temp.next; 
+        }
+    }
+
+    public static void main(String[] args) {
+        Node n1 = new Node(10);
+        Node n2 = new Node(20);
+        Node n3 = new Node(30);
+        Node n4 = new Node(40);
+        Node n5 = new Node(50);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        printLinkedList(n1);
+    }
+}
